5c6535380c2cb92e1b9a16d693b919e852ed7bf3
nzi19001@byui.edu <nzi19001@byui.edu>
2024-01-10 14:32:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code your solution here
+
+function saturdayFun(toDo = "roller-skate") {
+    return `This Saturday, I want to ${toDo}!`
+}
+
+
+function mondayWork(toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`
+}
+
+
+function wrapAdjective(adjective = "*") {
+    return function (word = "special") {
+        return `You are ${adjective}${word}${adjective}!`
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
